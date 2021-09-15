17b942fbe986e714aaff7aff57d305ca656349f5
NguyenTanHieu <90306455+NguyenTanHieu@users.noreply.github.com>
2021-09-15 18:02:34 +0700

    hihi

--- bai01/src/App.java
+++ bai01/src/App.java
@@ -1,8 +1,5 @@
 import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Fuck you");
         public static void main(String args[]) {
             Scanner sc =new Scanner(System.in);
         System.out.println("nhap ten n: ");
