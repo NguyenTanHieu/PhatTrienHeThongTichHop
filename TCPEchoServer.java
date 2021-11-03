55c6cefd04fdedb507f2141aefd370862bff224a
NguyenTanHieu <90306455+NguyenTanHieu@users.noreply.github.com>
2021-11-03 14:42:29 +0700

    Update TCPEchoServer.java

--- TCPEchoServer.java
+++ TCPEchoServer.java
@@ -1,39 +1,31 @@
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class TCPEchoServer {
-  public final static int serverPort=7;
-  public static void main(String[] args) {
+public class TPCEchoSever {
+   public static void main(String[] args) {
       try {
-          ServerSocket ss= new ServerSocket(serverPort);
-          System.out.println("Sever da duoc tao");
-          while(true){
-              try {
-                  Socket s=ss.accept();
-                  OutputStream os=s.getOutputStream();
-                  InputStream is=s.getInputStream();
-                  int ch= 0;
-                  while(true){
-                      ch=is.read();
-                      if(ch==-1)
-                      break;
-                      System.out.println((char)ch);
-                      os.write(ch);
-                  }
-                  s.close();
-                  } catch (IOException ie1) {
-                      System.out.println("Connection Error:"+ie1);
-                  //TODO: handle exception
-              }
-          }
-      } catch (IOException ie1) {
-          System.out.println("Server Creation Error:"+ie1);
+        ServerSocket ss= new ServerSocket(6789);
+        System.out.println("SEVER da duoc tao");
+        while(true){
+            Socket s= ss.accept();
+        InputStream is= s.getInputStream();
+        OutputStream os= s.getOutputStream();
+        int ch= 0;
+        while(true){
+            ch= is.read();
+            if(ch==-1)
+            break;
+            System.out.println((char)ch);
+            os.write(ch);
+        }
+    }
+      } catch (Exception e) {
           //TODO: handle exception
       }
-    }
-  }
+   }
+}
+
 
 
