06e300a1c1efeb62b64c361201202679f0e6eda4
NguyenTanHieu <90306455+NguyenTanHieu@users.noreply.github.com>
2021-11-03 14:41:56 +0700

    Update TCPEchoServer1.java

--- TCPEchoServer1.java
+++ TCPEchoServer1.java
@@ -1,27 +1,26 @@
-//   import java.io.IOException;
-//     import java.net.ServerSocket;
-//     import java.net.Socket;
-    
-//     class TPCEchoSever1{
-//         public final static int serverPort=8;
-//         public static void main(String[] args) {
-//            try {
-//                ServerSocket ss=new ServerSocket(serverPort);
-//                System.out.println("Server da duoc tao");
-//                while(true){
-//                    try {
-//                        Socket s=ss.accept();
-//                        RequestProcessing rp= new RequestProcessing(s);
-//                        rp.start();
-//                    } catch (IOException ie1) {
-//                        System.out.println("Connection Error:"+ie1);
-//                        //TODO: handle exception
-//                    }
-//                }
-//            } catch (IOException ie) {
-//                System.out.println("Server Creation Error:"+ie);
-//                //TODO: handle exception
-//            }
-//         }
-//     } 
+ import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPEchoSever1 {
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss = new ServerSocket(8888);
+            System.out.println("SEVER phuc vu song song");
+
+            while(true) {
+               try {
+                Socket s = ss.accept();
+                Processing p= new Processing(s);
+                p.start();
+               } catch (Exception e) {
+                   //TODO: handle exception
+               }
+                              
+            }
+
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+}
 
