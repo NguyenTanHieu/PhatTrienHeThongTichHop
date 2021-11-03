bc3f5cd5f51d62ed5a41e81d2d0b88e03f79e4af
NguyenTanHieu <90306455+NguyenTanHieu@users.noreply.github.com>
2021-11-03 14:42:51 +0700

    Update TCPEchoClient.java

--- TCPEchoClient.java
+++ TCPEchoClient.java
@@ -1,32 +1,30 @@
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
 public class TCPEchoClient {
-    public final static String serverIP="127.0.0.1";
-    public final static int severPort=7;
-    public static void main(String[] args) throws InterruptedException,IOException {
-        Socket s=null;
-        try {
-            s= new Socket(serverIP,severPort);
-            System.out.println("Client da duoc tao");
-            InputStream is=s.getInputStream();
-            OutputStream os=s.getOutputStream();
-            for(int i='0'; i<='9';i++){
-                os.write(i);
-                int ch=is.read();
-                System.out.println((char)ch);
-                Thread.sleep(2000);
-            }
-        } catch (IOException ie) {
-            System.out.println("Error:Can not create socket");
-            //TODO: handle exception
-        }finally{
-            if(s !=null){
-                s.close();
+        public static void main(String[] args) {
+            try {
+                Socket socket = new Socket("127.0.0.1", 6789);
+                System.out.println("Client was created");
+    
+                InputStream IS = socket.getInputStream();
+                OutputStream OS = socket.getOutputStream();
+    
+                for(int i ='0' ;  i<='9' ;i++) {
+                    OS.write(i);
+    
+                    int ch = IS.read();
+                    System.out.println((char) ch);
+    
+                    Thread.sleep(2000);
+                }
+                
+                if (socket!=null) 
+                    socket.close();
+                
+            } catch (Exception e) {
+                System.out.println(e.toString());
             }
         }
     }
-    }
-
